f16b764575a66e1f6917aded76b3386f0c92d709
Sam Hart ext:(%20%28sam%40pilot%29) <samx.hart@intel.com>
2010-11-04 16:06:38 -0700

    comment and stub dispatcher

--- lib/dispatcher.sh
+++ lib/dispatcher.sh
@@ -1,1 +1,11 @@
 # Command dispatcher
+
+# This is the general command processor.
+#
+# Its job is to take the incoming commands from the client agent
+# directory, interpret what they mean, and then dispatch them to the
+# various bit players around the Linux box which ultimately perform
+# those actions we need done (rebooting, domain join/unjoining, etc.)
+process_command() {
+    #
+}
