379c61df9b4f5853d7593ec59052aedc54b9f264
Sam Hart <samx.hart@intel.com>
2010-11-09 13:17:18 -0800

    Begin steward c application, add stubbed LICENSE file, add note about gSOAP legal requirements

---
+++ steward/eil_steward.c
@@ -0,0 +1,22 @@
+/*
+ * eil_steward.c
+ * -------------
+ * Description:
+ *  The steward is the thin gSOAP-based application which communicates
+ *  with the CCMS, accepts commands, and hands them to the dispatcher
+ *  (which is already installed on the system)
+ * License Note:
+ *  Please see the associated LICENSE file included with the source
+ *  repository of this application for special license considerations.
+ *  In a nutshell, parts of this application are based upon gSOAP, and
+ *  depending upon how it is built (dynamic vs. static linking) it may
+ *  also include GNU libc and other LGPL libraries. Care must be given
+ *  in redistributing this application, its binary and its source, as
+ *  redistribution outside of Intel will result in placing this software
+ *  under the GPL for compliance.
+ *
+ */
+
+#include "soapWSHttpBinding_USCOREIEILClientOperationsProxy.h"
+#include "WSHttpBinding_USCOREIEILClientOperations.nsmap"
+
