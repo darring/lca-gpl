8c5c268d5e8dfe14347efee1b048930d59b9da87
Sam Hart <samx.hart@intel.com>
2010-11-11 16:48:33 -0800

    silence the gSOAP parentheses warnings we can't do anything about

--- Globals.mk
+++ Globals.mk
@@ -3,5 +3,5 @@
 CC=gcc
 CPP=g++
 
-CFLAGS=-c -Wall -Wno-write-strings
-CP_FLAGS=-c -Wall -Wno-write-strings
+CFLAGS=-c -Wall -Wno-write-strings -Wno-parentheses
+CP_FLAGS=-c -Wall -Wno-write-strings -Wno-parentheses
