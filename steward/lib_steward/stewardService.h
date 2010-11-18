cae6501d803e486cbb5d86c36360f14050ed4275
Sam Hart <samx.hart@intel.com>
2010-11-18 14:30:47 -0800

    begin to flesh out the service wrapper header

--- steward/lib_steward/stewardService.h
+++ steward/lib_steward/stewardService.h
@@ -2,10 +2,38 @@
  * stewardService.h
  */
 
+/*! \mainpage Steward service wrapper for the EIL Linux Client Agent
+ *
+ * \section Introduction Introduction
+ *
+ * \section Usage Usage
+ *
+ */
+
 #ifndef stewardService_H
 #define stewardService_H
 
+// Nasty gSOAP bindings
+#include "soapWSHttpBinding_USCOREIEILClientOperationsProxy.h"
+#include "WSHttpBinding_USCOREIEILClientOperations.nsmap"
+#include "soapH.h"
 
-
+class StewardService
+{
+    private:
+        struct soap soap;
+        WSHttpBinding_USCOREIEILClientOperationsProxy service;
+    public:
+        //! Constructor for the Steward service wrapper
+        /*!
+         *
+         */
+        StewardService();
+        
+        //! Destructor for the Steward service wrapper
+        ~StewardService();
+        
+        
+}
 
 #endif
