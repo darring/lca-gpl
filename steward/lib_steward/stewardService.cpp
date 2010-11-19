a32c6da947b2be480c88f85eec764aebf49ab7f7
Sam Hart <samx.hart@intel.com>
2010-11-18 16:11:43 -0800

    destructor needs to tear down the soap instances

--- steward/lib_steward/stewardService.cpp
+++ steward/lib_steward/stewardService.cpp
@@ -18,3 +18,10 @@ StewardService::StewardService(StewardLogger *myLogger)
     // Initialize our soap runtime environment
     soap_init(&soap);
 }
+
+StewardService::~StewardService()
+{
+    soap_destroy(&soap); // remove deserialized class instances (C++ only)
+    soap_end(&soap); // clean up and remove deserialized data
+    soap_done(&soap); // detach environment (last use and no longer in scope)
+}
