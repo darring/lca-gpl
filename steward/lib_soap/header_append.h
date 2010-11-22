1666e79f4af04e9b280728dc095e68b5d91b6b64
Sam Hart <samx.hart@intel.com>
2010-11-22 15:50:42 -0800

    change the append file to reflect the current wsa changes

--- steward/lib_soap/header_append.h
+++ steward/lib_soap/header_append.h
@@ -1,15 +1,14 @@
 /*
  * The following is appended when we rebuild the gSOAP bindings
  */
-struct ns1__ReplyTo
-{
-    char *t__Address;
-};
-
+#import "wsa.h"
 struct SOAP_ENV__Header
 {
-   char *t__Action;
-   mustUnderstand char *t__MessageID;
-   mustUnderstand char *t__To;
-   ns1__ReplyTo *t__ReplyTo;
+    mustUnderstand _wsa__MessageID wsa__MessageID 0;
+    mustUnderstand _wsa__RelatesTo *wsa__RelatesTo 0;
+    mustUnderstand _wsa__From *wsa__From 0;
+    mustUnderstand _wsa__ReplyTo *wsa__ReplyTo 0;
+    mustUnderstand _wsa__FaultTo *wsa__FaultTo 0;
+    mustUnderstand _wsa__To wsa__To 0;
+    mustUnderstand _wsa__Action wsa__Action 0;
 };
