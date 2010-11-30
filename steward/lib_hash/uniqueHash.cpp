af79ea0b9314c7f0c03fab690342783d2a175832
Sam Hart <samx.hart@intel.com>
2010-11-30 13:06:01 -0800

    Add stubbed out uniqueHash class

---
+++ steward/lib_hash/uniqueHash.cpp
@@ -0,0 +1,27 @@
+/*
+ uniqueHash.cpp
+ ------------------
+ A basic helper class which provides unique hashes for the steward library
+ */
+
+#include "uniqueHash.h"
+
+UniqueHash::UniqueHash()
+{
+    //
+}
+
+UniqueHash::~UniqueHash()
+{
+
+}
+
+char* UniqueHash::GetHash()
+{
+
+}
+
+char* UniqueHash::GetHash(int seed)
+{
+
+}
---
+++ steward/lib_hash/uniqueHash.h
@@ -0,0 +1,44 @@
+/*
+ * UniqueHash Header
+ */
+
+/*! \mainpage Unique Hash generator for the EIL Client Agent Steward
+ *
+ * \section intro_sec Introduction
+ *
+ * This is a helper class which provides hash generation utilities for the
+ * Linux EIL Client Agent Steward.
+ */
+
+#ifndef uniqueHash_H
+#define uniqueHash_H
+
+class UniqueHash
+{
+    private:
+        const char[] base36chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public:
+        //! Constructor for the UniqueHash class
+        UniqueHash();
+
+        //! Destructor for the UniqueHash class
+        ~UniqueHash();
+
+        //! Get a unique hash
+        /*!
+         * The default GetHash method takes no arguments and returns a hash
+         * based upon the system time and a psuedo-random number.
+         */
+        char* GetHash();
+
+        //! Get a unique hash
+        /*!
+         * If GetHash is called with a seed integer, then this is used as a seed
+         * instead of system time and a unique hash is generated using this and
+         * a psuedo-random number.
+         * \param seed the integer to use as seed
+         */
+        char* GetHash(int seed);
+};
+
+#endif
