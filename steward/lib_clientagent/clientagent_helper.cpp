c602970c655689437afda3261b90d3c0837b9e3e
Sam Hart <samx.hart@intel.com>
2010-12-03 11:58:03 -0800

    Stub out a client agent helper interface (for interacting with the dispatcher script of the same name) so we don't have a lot of code repeat

---
+++ steward/lib_clientagent/clientagent_helper.cpp
@@ -0,0 +1,23 @@
+/*
+   clientagent_helper.cpp
+   ----------
+   Interface for the client agent helper script provided by the dispatcher
+ */
+
+#include "clientagent_helper.h"
+
+ClientAgentHelper::ClientAgentHelper()
+{
+
+}
+
+ClientAgentHelper::~ClientAgentHelper()
+{
+
+}
+
+void ClientAgentHelper::Get(
+    char *result, int size, ClientAgentOptions option)
+{
+
+}
---
+++ steward/lib_clientagent/clientagent_helper.h
@@ -0,0 +1,51 @@
+/*
+ * ClientAgentHelper header
+ */
+
+/*! \mainpage ClientAgentHelper Interface
+ *
+ * \section intro_sec Introduction
+ *
+ * This class provides the interface to the client agent helper scripts provided
+ * by the dispatcher.
+ *
+ * \section TODO_sec TODO
+ *
+ */
+
+#ifndef clientagent_helper_H
+#define clientagent_helper_H
+
+//! Possible options for the client agent
+enum ClientAgentOptions
+{
+    CCMSLOG,
+    PIDFILE,
+};
+
+//! Interface for the client agent helper script
+class ClientAgentHelper
+{
+    private:
+        FILE *filePipe;
+        char *scriptCommand;
+    public:
+        //! Constructor for the client agent helper
+        ClientAgentHelper();
+
+        //! Destructor for the client agent helper
+        ~ClientAgentHelper();
+
+        //! Get a result from the client agent helper
+        /*!
+         * Given an option, will obtain a result from the client agent helper
+         * script.
+         *
+         * \param result The resultant character string
+         * \param size The size of result
+         * \param option The client agent option
+         */
+        void Get(char *result, int size, ClientAgentOptions option);
+};
+
+#endif
