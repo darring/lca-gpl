f8cbd7f40ca4f10da8e1ccd943fee553b5311ebd
Sam Hart <samx.hart@intel.com>
2010-12-06 10:23:49 -0800

    signal handling setup

--- steward/sig_handlers/sighandlers.cpp
+++ steward/sig_handlers/sighandlers.cpp
@@ -12,7 +12,46 @@ void setupSignalHandlers()
     static bool hasSetup = false;
     if(!hasSetup)
     {
-        struct sigaction act_SIGHUP, act_SIGINT, act_SIGTERM;
+        struct sigaction act_SIGHUP, act_SIGINT, act_SIGTERM, act_OLD;
+
+        // Set up SIGHUP
+        act_SIGHUP.sa_handler = handle_SIGHUP;
+        sigemptyset (&act_SIGHUP.sa_mask);
+        act_SIGHUP.sa_flags = 0;
+
+        sigaction (SIGHUP, NULL, &act_OLD);
+        if (act_OLD.sa_handler != SIG_IGN)
+            sigaction (SIGHUP, &act_SIGHUP, NULL);
+        else
+        {
+            sigaction (SIGHUP, &act_SIGHUP, &act_OLD);
+        }
+
+        // Set up SIGINT
+        act_SIGINT.sa_handler = handle_SIGINT;
+        sigemptyset (&act_SIGINT.sa_mask);
+        act_SIGINT.sa_flags = 0;
+
+        sigaction (SIGINT, NULL, &act_OLD);
+        if (act_OLD.sa_handler != SIG_IGN)
+            sigaction (SIGINT, &act_SIGINT, NULL);
+        else
+        {
+            sigaction (SIGINT, &act_SIGINT, &act_OLD);
+        }
+
+        // Set up SIGTERM
+        act_SIGTERM.sa_handler = handle_SIGTERM;
+        sigemptyset (&act_SIGTERM.sa_mask);
+        act_SIGTERM.sa_flags = 0;
+
+        sigaction (SIGTERM, NULL, &act_OLD);
+        if (act_OLD.sa_handler != SIG_IGN)
+            sigaction (SIGTERM, &act_SIGTERM, NULL);
+        else
+        {
+            sigaction (SIGTERM, &act_SIGTERM, &act_OLD);
+        }
     }
 }
 
