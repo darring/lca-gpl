ef77283b6670e6de429e220163d7937378bbc0cc
Sam Hart <samx.hart@intel.com>
2010-12-06 09:32:50 -0800

    flesh out the signal handler header

--- steward/sig_handlers/sighandlers.h
+++ steward/sig_handlers/sighandlers.h
@@ -8,9 +8,47 @@
  *
  * This is a collection of signal handler functions for the EIL Linux Client
  * Agent.
+ * \section signals_sec Handled Signals
+ *
+ * The steward interprets the following signals in the following ways:
+ \htmlonly
+
+    <table border="0">
+    <tr>
+    <td>SIGHUP</td><td>Causes steward to exit gracefully.</td>
+    <td>SIGINT</td><td>Causes steward to exit gracefully.</td>
+    <td>SIGTERM</td><td>Causes steward to exit quickly (gracefully if possible)</td>
+    </tr>
+    </table>
+
+ \endhtmlonly
  */
 
-#ifndef uniqueHash_H
-#define uniqueHash_H
+#ifndef sigHandlers_H
+#define sigHandlers_H
 
+//! Call to set up the signal handlers
+/*!
+ * This function should be called once per application execution to set up the
+ * signal handlers. If it is called more than once, it will fail silently.
+ */
+void setupSignalHandlers();
+
+//! Handle the SIGHUP signal
+/*! Should not be called except by sigaction!
+ * \param sig The signal
+ */
+void handle_SIGHUP(int sig);
+
+//! Handle the SIGINT signal
+/*! Should not be called except by sigaction!
+ * \param sig The signal
+ */
+void handle_SIGINT(int sig);
+
+//! Handle the SIGTERM signal
+/*! Should not be called except by sigaction!
+ * \param sig The signal
+ */
+void handle_SIGTERM(int sig);
 #endif
