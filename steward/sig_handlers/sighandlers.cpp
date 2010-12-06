a9d80ad8ace2d5a0c61cc3de81a14040b80aae45
Sam Hart <samx.hart@intel.com>
2010-12-06 09:04:58 -0800

    stub out signal handler files

---
+++ steward/sig_handlers/sighandlers.cpp
@@ -0,0 +1,7 @@
+/*
+ sighandlers.cpp
+ ------------------
+ Collection of signal handlers for EIL Linux Client Agent (steward)
+ */
+
+#include "sighandlers.h"
---
+++ steward/sig_handlers/sighandlers.h
@@ -0,0 +1,16 @@
+/*
+ * sighandlers Header
+ */
+
+/*! \mainpage Signal Handler functions for the EIL Linux Client Agent
+ *
+ * \section intro_sec Introduction
+ *
+ * This is a collection of signal handler functions for the EIL Linux Client
+ * Agent.
+ */
+
+#ifndef uniqueHash_H
+#define uniqueHash_H
+
+#endif
