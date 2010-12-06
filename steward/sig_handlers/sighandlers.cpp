82522b87ee41b3900e9f8eab096ed5e87e647ca7
Sam Hart <samx.hart@intel.com>
2010-12-06 09:57:56 -0800

    more work on signal handling

--- steward/sig_handlers/sighandlers.cpp
+++ steward/sig_handlers/sighandlers.cpp
@@ -5,9 +5,15 @@
  */
 
 #include "sighandlers.h"
+#include <signal.h>
 
 void setupSignalHandlers()
 {
+    static bool hasSetup = false;
+    if(!hasSetup)
+    {
+        struct sigaction act_SIGHUP, act_SIGINT, act_SIGTERM;
+    }
 }
 
 void handle_SIGHUP(int sig)
