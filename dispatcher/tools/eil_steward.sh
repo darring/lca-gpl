4703152094dd59358c96b9aac68a390f4f45473e
Sam Hart <samx.hart@intel.com>
2010-12-06 16:02:07 -0800

    * Further work on steward init script * Purge the lsb init function requirement (we only used it for logging)

--- dispatcher/tools/eil_steward.sh
+++ dispatcher/tools/eil_steward.sh
@@ -6,8 +6,6 @@
 
 PATH="${PATH}:/sbin"
 
-. /lib/lsb/init-functions
-
 check_steward_running() {
     if [ -e "/opt/intel/eil/clientagent/home/client-agent.pid" ]; then
         # Verify that it's already running
@@ -32,9 +30,9 @@ start)
     local _STATUS=$?
     if [ "${_STATUS}" -eq "0" ]; then
         # According to the LSB, this is considered a success
-        log_warning_msg "eil_steward already running!"
-        log_warning_msg "If you think this is a mistake, check the pid file and"
-        log_warning_msg "associated process..."
+        echo "eil_steward already running!"
+        echo "If you think this is a mistake, check the pid file and"
+        echo "associated process..."
     elif [ "${_STATUS}" -eq "1" ]; then
         # Service is not running, but pid file exists, let's kill old file
         # and restart
@@ -59,17 +57,57 @@ stop)
         # Service is not running, but pid file exists
         unlink /opt/intel/eil/clientagent/home/client-agent.pid
     else
-        log_warning_msg "eil_steward is not running."
+        echo "eil_steward is not running."
     fi
+
+    exit 0
     ;;
 retstart|try-restart|reload|force-reload)
     # Restart the steward service
+    check_steward_running
+    local _STATUS=$?
+    if [ "${_STATUS}" -eq "0" ]; then
+        # Send it SIGHUP
+        local PID1=$(cat /opt/intel/eil/clientagent/home/client-agent.pid)
+        kill -1 ${PID1}
+
+        # Give it a bit to stop what it was doing
+        sleep 5
+        eil_steward
+    elif [ "${_STATUS}" -eq "1" ]; then
+        # Service is not running, but pid file exists
+        unlink /opt/intel/eil/clientagent/home/client-agent.pid
+        eil_steward
+    else
+        # Wasn't running, just restart
+        eil_steward
+    fi
+
+    exit 0
     ;;
 status)
     # Display status
+    check_steward_running
+
+    local _STATUS=$?
+    if [ "${_STATUS}" -eq "0" ]; then
+        # It's running
+        local PID1=$(cat /opt/intel/eil/clientagent/home/client-agent.pid)
+        echo "eil_steward start/running, process ${PID1}"
+    elif [ "${_STATUS}" -eq "1" ]; then
+        echo "eil_steward not running, pid file exists"
+    else
+        echo "eil_steward not running"
+    fi
+
+    exit $_STATUS
     ;;
 *)
     # Display usage options
+    echo "Usage: /etc/init.d/eil_steward.sh {start|stop|status|restart}"
+
+    exit 0
     ;;
 esac
 
+exit 0
