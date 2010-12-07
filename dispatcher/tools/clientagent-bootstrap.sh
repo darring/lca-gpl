3ca9b1b4fda99bdaa3864237f695d8b70a908bc4
Sam Hart <samx.hart@intel.com>
2010-12-07 10:40:54 -0800

    more work on the bootstrap script

--- dispatcher/tools/clientagent-bootstrap.sh
+++ dispatcher/tools/clientagent-bootstrap.sh
@@ -3,3 +3,41 @@
 # The client agent bootstrap script
 #----------------------------------
 # This script is intended to bootstrap the EIL Linux Client Agent
+#
+# This script must be completely self-contained (unfortunately)
+
+# EIL IP configs
+EIL_AUTO="172.16.3.10"
+EIL_STAGING="10.4.0.26"
+
+# This function cleans the previous hosts file
+clean_hosts_file() {
+    local TMP_FILE=`mktemp`
+    sed '/## EIL_BEGIN/,/## EIL_END/d' /etc/hosts > ${TMP_FILE}
+    cp -f ${TMP_FILE} /etc/hosts
+    rm -f ${TMP_FILE}
+}
+
+# This function makes the hosts file, it backs up the old hosts file, and
+# stores checksum signatures to check for tampering
+make_hosts_file() {
+    local DATE_STAMP=$(date +%j%H%M%S)
+    cp -f /etc/hosts /etc/hosts.backup.%{DATE_STAMP}
+    md5sum /etc/hosts.backup.%{DATE_STAMP} > /etc/hosts.backup.md5.%{DATE_STAMP}
+
+    clean_hosts_file
+
+    echo "## EIL_BEGIN" >> /etc/hosts
+    echo "### EIL Linux Client Agent Specific Config ###" >> /etc/hosts
+    echo "# Changes to this section will be overwritten during client agent upgrades" >> /etc/hosts
+    echo "${EIL_AUTO}    eilauto01   eilauto01.eil-infra.com" >> /etc/hosts
+    echo "${EIL_STAGING} eilstaging  eilstaging.eil-infra.com" >> /etc/hosts
+    echo "## EIL_END" >> /etc/hosts
+
+    md5sum /etc/hosts > /etc/hosts.md5
+}
+
+# Start out by setting up our hosts file
+make_hosts_file
+
+# Obtain the latest release
