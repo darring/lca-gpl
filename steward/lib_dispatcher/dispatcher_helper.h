437f4621b99d8aa408d59e59a5b690cdae7a6557
Sam Hart <samx.hart@intel.com>
2010-12-21 16:29:28 -0800

    stubbed dispatcher helper

---
+++ steward/lib_dispatcher/dispatcher_helper.h
@@ -0,0 +1,20 @@
+/*
+ * DispatcherHelper header
+ */
+
+/*! \mainpage DispatcherHelper Interface
+ *
+ * \section intro_sec Introduction
+ *
+ * This class provides an abstract interface into the dispatcher itself. It is
+ * intended to centralize all dispatcher-related calls and interfacing into a
+ * single location such that any changes only affect one location.
+ *
+ * \section TODO_sec TODO
+ *
+ */
+
+#ifndef dispatcher_helper_H
+#define dispatcher_helper_H
+
+#endif
