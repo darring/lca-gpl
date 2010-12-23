1f632a5f0f1d03f82705af5f78bf8bb8b59546f6
Sam Hart <samx.hart@intel.com>
2010-12-23 09:03:14 -0800

    refine the elevate script command a bit so we don't need to pass it a UID (we can get that from ownership on the file)

--- elevate_script/elevate_script.cpp
+++ elevate_script/elevate_script.cpp
@@ -2,20 +2,25 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
-// This elevates scripts to a given UID
+// This elevates scripts to the UID which owns the script
 // usage:
-// elevate_script UID /foo/bar/script.sh
+// elevate_script /foo/bar/script.sh
 int main(int argc, char *argv[])
 {
-    if(argc != 3) {
+    if(argc != 2) {
         // We must be run with two arguments!
         printf("ERROR! %i\n", argc);
         return -1;
     }
 
-    int UID = atoi(argv[1]);
-    char *script = argv[2];
+    char *script = argv[1];
+
+    // Find the UID
+    struct stat *buf
+    stat(script, buf);
+    int UID buf->st_uid;
 
     setuid( UID );
     system( script );
