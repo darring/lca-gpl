d59695d8f7174a7696c8a9e4f32458ee96947b39
Sam Hart <samx.hart@intel.com>
2011-01-11 13:13:02 -0800

    add a stubbed service wrapper for accessing the NMSA

---
+++ steward/lib_nmsa/nmsaService.h
@@ -0,0 +1,33 @@
+/*
+ * nmsaService.h
+ */
+
+/*! \mainpage NMSA service wrapper for the EIL Linux Client Agent
+ *
+ * \section Introduction Introduction
+ *
+ * \section Usage Usage
+ *
+ */
+
+#ifndef nmsaService_H
+#define nmsaService_H
+
+class StewardLogger;
+
+class NMSA_Service
+{
+    private:
+        StewardLogger *logger;
+    public:
+        //! Constructor for the NMSA service wrapper
+        /*!
+         * \param myLogger is the logger instance we should use
+         */
+        NMSA_Service(StewardLogger *myLogger);
+
+        //! Destructor for the NMSA service wrapper
+        ~NMSA_Service();
+};
+
+#endif
