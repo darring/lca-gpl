61e6ddcd58d7b332f021d5ee94379395ee6e8f1e
Sam Hart <samx.hart@intel.com>
2011-01-11 16:29:07 -0800

    begin to flesh out the setup env script now that the deps are solved

--- build_env/setup_env.sh
+++ build_env/setup_env.sh
@@ -16,10 +16,44 @@ automake
 autoconf
 autotools-dev
 byacc
+bison
+libssl-dev
 EOF
 )
 
 # Check we're running debian/ubuntu
+if [ ! -f "/etc/debian_version" ]; then
+    cat <<EOF
+
+ This script must be run in a Debian-derived distribution (such as Ubuntu)!
+
+ If you wish to build the EIL Linux client agent in another distribution, please
+ see the BUILD_ENV text file and the source for this script to find the proper
+ dependencies necessary to set up your build environment!
+
+EOF
+
+exit 1
+fi
+
+# Show interactive text explaining what we're about to do
+cat <<EOF
+
+ This script will set up a stock Debian or Ubuntu install for development and
+ building of the EIL Linux client agent.
+
+ If you are not using Debian or Ubuntu, then take a look at this script (and the
+ BUILD_ENV text file) for more information about the EIL Linux client agent's
+ requirements. If you are building or developing on a distribution other than
+ Debian or Ubuntu, it is left up to you to figure out what is needed for a proper
+ development environment.
+
+ This script will have a number of interactive sections, please read everything
+ carefully and answer the questions with caution.
+
+EOF
+
+read -p "Press [Enter] to continue : "
 
 # Install the dependencies
 
@@ -28,6 +62,7 @@ EOF
 # configure and checkinstall
 
 configure
+make
 checkinstall
 
 # vim:set ai et sts=4 sw=4 tw=80:
