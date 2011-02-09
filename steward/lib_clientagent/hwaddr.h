80f88480e094ec98016506d523486258aba22beb
Sam Hart <samx.hart@intel.com>
2011-02-09 11:44:38 -0800

    stub hwaddr items

---
+++ steward/lib_clientagent/hwaddr.h
@@ -0,0 +1,20 @@
+/*
+ * Hardware Address tools header
+ */
+
+/*! \mainpage Hardware Address Tools
+ *
+ * \section intro_sec Introduction
+ *
+ * This file provides interfaces which abstract access to the hardware address
+ * for the EIL Linux client agent.
+ *
+ * \section usage_sec Usage
+ *
+ */
+
+#ifndef hwaddr_h
+#define hwaddr_h
+
+
+#endif
