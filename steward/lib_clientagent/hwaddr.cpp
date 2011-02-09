c063b75a7e18dcbcd02a34c2e1074860d32ab390
Sam Hart <samx.hart@intel.com>
2011-02-09 12:00:27 -0800

    stub cpp file

---
+++ steward/lib_clientagent/hwaddr.cpp
@@ -0,0 +1,19 @@
+/*
+    hwaddr.cpp
+    ----------
+    Hardware address tools
+ */
+
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <string.h>
+
+#include "hwaddr.h"
+
+void getHwAddr(char *hwaddr)
+{
+
+}
