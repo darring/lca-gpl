c92fca824717587c037382164d6eea263cace2bc
Sam Hart <samx.hart@intel.com>
2011-02-09 11:57:21 -0800

    stub out interface

--- steward/lib_clientagent/hwaddr.h
+++ steward/lib_clientagent/hwaddr.h
@@ -16,5 +16,14 @@
 #ifndef hwaddr_h
 #define hwaddr_h
 
+//! Obtains the lowest order hardware address
+/*!
+ * When called, will attempt to run through the potential network interfaces
+ * on the system and return a string for the hardware address of the lowest
+ * order interface.
+ *
+ * \param hwaddr The char pointer for the hardware address string
+ */
+void getHwAddr(char *hwaddr);
 
 #endif
