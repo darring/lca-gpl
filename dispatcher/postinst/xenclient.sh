a55736e02506c72df97ba364c63dff2d23cdd67e
Sam Hart <samx.hart@intel.com>
2011-02-10 09:15:16 -0800

    begin work on a post-install process (which right now, only xenclient needs)

---
+++ dispatcher/postinst/xenclient.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# XenClient Specific post installation tool
+# -----------------------------------------
+# This post-install script fixes various platform specific problems that occur
+# during the install for XenClient systems
