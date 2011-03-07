5b060d23cc89d9d774af60e8064ee42521bb3a92
Sam Hart <samx.hart@intel.com>
2011-03-07 13:29:30 -0800

    stub out the unit tests for the asset helper
    --HG--
    branch : bigdev

---
+++ dispatcher/tests/asset-helper-verify.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# asset-helper-verify.sh
+# ----------------------
+# Verifies the asset helper section of the code is in working condition
+
+
+
+. /usr/share/shunit2/shunit2
+
+# vim:set ai et sts=4 sw=4 tw=80:
+
