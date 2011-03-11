4c95ada43bdd738fe8c6280e953522285b51ff20
Sam Hart <samx.hart@intel.com>
2011-03-10 16:20:44 -0800

    and use it here too
    --HG--
    branch : bigdev

--- steward/lib_steward/stewardService.cpp
+++ steward/lib_steward/stewardService.cpp
@@ -306,54 +306,10 @@ void StewardService::queryForClientCommands_byHWAddr(
             returnCommand->Command = NO_COMMAND;
             logger->QuickLog("StewardService> No command");
         } else {
-            /*
-            Be sure to update the header with the proper HTTP SOAP
-            action! (or else we will get an "ActionMismatch" error)
-            */
-            header.wsa5__Action = EIL__UPDATECOMMANDSTATUS;
-            synHeaders();
-
-            /*
-            First we need to get our responses ready
-            */
-            _ns1__UpdateCommandStatus updateCmdStat;
-            ns4__EILCommand cmd;
-            _ns1__UpdateCommandStatusResponse updateCmdStatResp;
-
-            updateCmdStat.ctx = ctx;
-
-            // Parse *what* our command was
-            if(strcasecmp(
-                response.GetCommandToExecuteUsingMacAddressResult->CommandName,
-                CCMS_REBOOT) == 0)
-            {
-                currentState = STATE_ExecutingCommand;
-                returnCommand->ReturnState = COMMAND_SUCCESS;
-                returnCommand->Command = REBOOT;
-
-                ns4__EILCommandStatus complete =
-                    ns4__EILCommandStatus__COMMAND_USCOREEXECUTION_USCORECOMPLETE;
-                int errorcode = 0;
-                cmd.CommandResult = "Reboot Successful";
-                cmd.CommandStatus = &complete;
-                cmd.ErrorCode = &errorcode;
-                cmd.CommandName =
-                    response.GetCommandToExecuteUsingMacAddressResult->CommandName;
-
-                updateCmdStat.cmd = &cmd;
-                updateCmdStat.cmd->OperationID =
-                    response.GetCommandToExecuteUsingMacAddressResult->OperationID;
-
-                // FIXME - Do we want to deal with op_codes here as well?
-                service.UpdateCommandStatus(
-                    &updateCmdStat, &updateCmdStatResp);
-            } // Other commands go here
-            else
-            {
-                currentState = STATE_None;
-                returnCommand->ReturnState = COMMAND_ERROR;
-                returnCommand->Command = NO_COMMAND;
-            }
+            // FIXME - Do we want to deal with op_codes here as well?
+            parseCommandFromCCMS(ctx,
+                response.GetCommandToExecuteUsingMacAddressResult,
+                returnCommand);
         }
     } else {
         parseOpCode(returnCommand);
