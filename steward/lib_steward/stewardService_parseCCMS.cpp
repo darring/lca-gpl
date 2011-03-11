1059e9ca10a25ac8755057ae5c4dc4329f4e84eb
Sam Hart <samx.hart@intel.com>
2011-03-10 16:05:37 -0800

    stub
    --HG--
    branch : bigdev

---
+++ steward/lib_steward/stewardService_parseCCMS.cpp
@@ -0,0 +1,14 @@
+/*
+ * Parsing of the CCMS commands
+ */
+
+#include "logger.h"
+#include "stewardService.h"
+#include "EIL_defines.h"
+
+int StewardService::parseCommandFromCCMS(
+            ns4__EILCommand *EILCommand,
+            CCMS_Command *returnCommand)
+{
+
+}
