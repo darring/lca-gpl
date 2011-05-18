f80017bd2b50bfde6ceca714fe780e764a1ecc9d
Sam Hart <hartsn@gmail.com>
2011-05-18 15:18:12 -0400

    more work on the chroot wrapper

--- build_env/chroot_wrap.sh
+++ build_env/chroot_wrap.sh
@@ -15,6 +15,18 @@ fi
 # Get our chroot path
 if [ -n "$1" ]; then
     CHROOT_PATH=$1
+
+    # Check on the chroot path
+    if [ -d "$CHROOT_PATH" ]; then
+            # Mount our dev and proc
+            mount --bind /proc ${CHROOT_PATH}/proc
+            mount --bind /dev ${CHROOT_PATH}/dev
+
+            chroot ${CHROOT_PATH}
+    else
+        echo "The chroot path '${CHROOT_PATH}' does not exist or is not a directory!"
+        exit 1
+    if
 fi
 
 # vim:set ai et sts=4 sw=4 tw=80:
