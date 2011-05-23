b088834f105f8536422dc09c4f938ad003517561
Sam Hart <hartsn@gmail.com>
2011-05-23 13:38:08 -0400

    laf test script

---
+++ dispatcher/postinst/laf_test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# laf_test.sh
+# -----------
+# Test script to determine if the system should run the laf post install
+# scripts
+
+# THIS MUST ONLY BE RUN BY THE DISPATCHER INSTALL SCRIPT
+
+# Load our libraries
+. /opt/intel/eil/clientagent/lib/helper.sh
+. /opt/intel/eil/clientagent/lib/globals.sh
+
+# TODO
+
+# vim:set ai et sts=4 sw=4 tw=80:
