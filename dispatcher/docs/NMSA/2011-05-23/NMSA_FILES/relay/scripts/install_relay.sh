1602586f35e12a945243ffa4aa79160289519cb0
Sam Hart <hartsn@gmail.com>
2011-05-23 10:58:30 -0400

    add relay files as sent from stewart

---
+++ dispatcher/docs/NMSA/2011-05-23/NMSA_FILES/relay/scripts/install_relay.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+INSTALL_DIR=/opt/intel/eil/relay
+CREATE_DIR=("bin")
+REQ_PROGS=()
+
+has_cmd() {
+ ret=0;
+ cmd=$1
+ shift
+ tmp_f=/tmp/`date +%m%d%y-%M%S`
+ whereis $cmd > $tmp_f
+ if [ ! $? -eq 0 ]
+ then
+  logerr $client 6 "+1"
+  return 1
+ fi
+ str=`cat $tmp_f | awk '{print $2}'`
+ if [ ! -n "$str" ]
+ then
+  ret=1
+ fi
+ rm $tmp_f
+ return $ret
+}
+
+check_req_progs() {
+ ret=0
+ for prog in "${REQ_PROGS[@]}"; do
+  has_cmd $prog
+  if [ $? -eq 1 ]; then
+   echo "Missing required program $prog.  Please install and then try again"
+   ret=1
+   break
+  fi
+ done
+ return $ret
+} 
+
+create_dir() { 
+ if [ ! -d $INSTALL_DIR ]; then
+  if [ ! -d /opt/intel/eil ]; then
+   if [ ! -d /opt/intel ]; then
+    mkdir /opt/intel
+   fi
+   mkdir /opt/intel/eil
+  fi
+  mkdir $INSTALL_DIR
+ fi
+ 
+ for dir in "${CREATE_DIR[@]}"; do
+  if [ ! -d $INSTALL_DIR/$dir ]; then
+   mkdir $INSTALL_DIR/$dir
+  fi
+ done
+}
+
+LUID=$(id -u)
+if [[ $LUID -ne 0 ]]; then
+ echo "Please run install script as root"
+ exit 1
+fi
+
+
+check_req_progs
+if [ ! $? -eq 0 ]; then
+ echo "Please install missing programs and then run installer again"
+ exit 1
+fi
+create_dir
+./scripts/create_relay_sh.sh
+cp ./src/relay.sh $INSTALL_DIR/bin
+./scripts/install_cron.sh
