0c2059c15bca10d7ee93e82aef53692fa3486da6
Sam Hart <hartsn@gmail.com>
2011-05-31 18:31:29 -0400

    proper import

--- dispatcher/lib/nmsa_conf.py
+++ dispatcher/lib/nmsa_conf.py
@@ -1,20 +1,21 @@
 import os
 import ConfigParser as configparser
 
-def setup_config(conf_file):
-    config = configparser.RawConfigParser()
-    if os.path.isfile(conf_file):
-        config.read(conf_file)
-    else:
-        # No file exists, make a sensible default
-        config.add_section('main')
-        config.set('main', 'log_level', '3')
-        config.set('main', 'sleep_timer', '30')
+class SetupConf:
+    def setup_config(conf_file):
+        config = configparser.RawConfigParser()
+        if os.path.isfile(conf_file):
+            config.read(conf_file)
+        else:
+            # No file exists, make a sensible default
+            config.add_section('main')
+            config.set('main', 'log_level', '3')
+            config.set('main', 'sleep_timer', '30')
 
-        # Writing our configuration file to 'example.cfg'
-        with open(config_file, 'wb') as configfile:
-            config.write(configfile)
+            # Writing our configuration file to 'example.cfg'
+            with open(config_file, 'wb') as configfile:
+                config.write(configfile)
 
-    return config
+        return config
 
 # vim:set ai et sts=4 sw=4 tw=80:
