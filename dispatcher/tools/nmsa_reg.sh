0ed7860d32746d174b90dd43cbd97a30b263293b
Sam Hart <hartsn@gmail.com>
2011-05-31 11:34:23 -0400

    whitespace police

--- dispatcher/tools/nmsa_reg.sh
+++ dispatcher/tools/nmsa_reg.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# nmsa_reg.sh
+# -----------
+# Generates and returns a properly formed NMSA registration URL of the form:
 # $NMSA_SERVER/nmsa/client_register.php?mac=m&hostname=h&bmc=b&bridge=B&trans=t
 
 CHAN_ARRAY=("0" "6");
@@ -13,44 +16,45 @@ BRUTE_WORKED=0
 MAC_WORKED=0
 
 get_mac() {
- for i in {0..25} ; do
-  /sbin/ifconfig eth$i > tmp.f
-  if ! grep "Device not found" tmp.f; then
-   MAC=`ifconfig eth$i | grep HWaddr | awk '{print $5}'`
-   MAC_WORKED=1
-   break
-  fi
- done
- rm tmp.f
+    for i in {0..25} ; do
+        /sbin/ifconfig eth$i > tmp.f
+        if ! grep "Device not found" tmp.f; then
+            MAC=`ifconfig eth$i | grep HWaddr | awk '{print $5}'`
+            MAC_WORKED=1
+            break
+        fi
+    done
+    rm tmp.f
 }
 
 get_hostname() {
- HOSTNAME=`hostname`
+    HOSTNAME=`hostname`
 }
 
 get_bmc_ip() {
- BMC_IP=`ipmitool lan print | grep "IP Address" | grep -v Source | awk '{print $4}'`
+    BMC_IP=`ipmitool lan print | grep "IP Address" | grep -v Source | \
+        awk '{print $4}'`
 }
 
 get_chan_tran() {
- control=0;
- for chan in "${CHAN_ARRAY[@]}"; do
-  if [ $control -eq 1 ]; then
-   break;
-  fi
-  for tran in "${TRAN_ARRAY[@]}"; do
-   res=`ipmitool -b $chan -t 0x$tran mc info 2>&1`;
-   if grep -q Timeout <<< $res &> /dev/null; then
-    continue
-   else
-    CHAN=$chan
-    TRAN=$tran
-    control=1
-    BRUTE_WORKED=1
-    break;
-   fi
-  done
- done
+    control=0;
+    for chan in "${CHAN_ARRAY[@]}"; do
+        if [ $control -eq 1 ]; then
+            break;
+        fi
+        for tran in "${TRAN_ARRAY[@]}"; do
+            res=`ipmitool -b $chan -t 0x$tran mc info 2>&1`;
+            if grep -q Timeout <<< $res &> /dev/null; then
+                continue
+            else
+                CHAN=$chan
+                TRAN=$tran
+                control=1
+                BRUTE_WORKED=1
+                break;
+            fi
+        done
+    done
 }
 
 get_mac
@@ -59,7 +63,9 @@ get_bmc_ip
 get_chan_tran
 
 if [[ $BRUTE_WORKED -eq 1 && $MAC_WORKED -eq 1 ]]; then
- echo "/nmsa/client_register.php?mac=$MAC&hostname=$HOSTNAME&bmc=$BMC_IP&bridge=$CHAN&trans=$TRAN"
+    echo "/nmsa/client_register.php?mac=$MAC&hostname=$HOSTNAME&bmc=$BMC_IP&bridge=$CHAN&trans=$TRAN"
 else
- echo "ERROR"
+    echo "ERROR"
 fi
+
+# vim:set ai et sts=4 sw=4 tw=80:
