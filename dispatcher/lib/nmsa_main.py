d64a8e38d430009ec025600c893219aca8324ef3
Sam Hart <hartsn@gmail.com>
2011-06-01 11:52:43 -0400

    master nmsa class

---
+++ dispatcher/lib/nmsa_main.py
@@ -0,0 +1,12 @@
+'''
+nmsa_main.py
+------------
+The main daemon loop controller class and functionality
+'''
+
+class NMSA_Master:
+
+    self.__init__(self):
+        #
+
+# vim:set ai et sts=4 sw=4 tw=80:
