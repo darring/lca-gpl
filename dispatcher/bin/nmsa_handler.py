78187285b2f86b760c9eb9379f385f8c5be87358
Sam Hart <hartsn@gmail.com>
2011-06-01 11:37:02 -0400

    toggle file check

--- dispatcher/bin/nmsa_handler.py
+++ dispatcher/bin/nmsa_handler.py
@@ -24,6 +24,9 @@ from nmsa_conf import setup_conf
 '''The minimum timer resolution for main run loop'''
 __min_timer_res = 10
 
+'''The file toggle that determines if we should run or not'''
+__nmsa_toggle = '/opt/intel/eil/clientagent/home/.nmsa_enable'
+
 class HandlerDaemon(Daemon):
     __log_file = '/opt/intel/eil/clientagent/home/nmsa_handler.log'
     __conf_file = '/opt/intel/eil/clientagent/home/nmsa_handler.cfg'
@@ -76,19 +79,24 @@ def usage():
 
 if __name__ == "__main__":
     daemon = HandlerDaemon('/opt/intel/eil/clientagent/home/nmsa_handler.pid')
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
+    if os.path.isfile(__nmsa_toggle):
+        if len(sys.argv) == 2:
+            if 'start' == sys.argv[1]:
+                daemon.start()
+            elif 'stop' == sys.argv[1]:
+                daemon.stop()
+            elif 'restart' == sys.argv[1]:
+                daemon.restart()
+            else:
+                print "Unknown command"
+                sys.exit(2)
+            sys.exit(0)
         else:
-            print "Unknown command"
+            usage()
             sys.exit(2)
-        sys.exit(0)
     else:
         usage()
+        print "\nThe file:\n\n\t'%s'\n\nmust exist before running this daemon" % __nmsa_toggle
         sys.exit(2)
 
 # vim:set ai et sts=4 sw=4 tw=80:
