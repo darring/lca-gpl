b32f37b5580cf86d613d56fb9a2c23257cc84aa5
Sam Hart <hartsn@gmail.com>
2011-06-02 17:04:09 -0400

    must deal with init script here

--- dispatcher/postinst/install_laf.sh
+++ dispatcher/postinst/install_laf.sh
@@ -4,6 +4,10 @@
 # --------------
 # Install and set up the LAF scripts as a postinst process
 
+# Load our libraries
+. /opt/intel/eil/clientagent/lib/helper.sh
+. /opt/intel/eil/clientagent/lib/globals.sh
+
 # THIS MUST ONLY BE RUN BY THE DISPATCHER INSTALL SCRIPT
 
 INSTALL_CWD=$1
@@ -35,6 +39,9 @@ clean_up() {
     rmdir --ignore-fail-on-non-empty $INSTALL_DIR
 }
 
+clean_init() {
+}
+
 create_dir() {
     if [ ! -d $INSTALL_DIR ]; then
         mkdir -p $INSTALL_DIR
@@ -57,10 +64,31 @@ install_files() {
     done
 }
 
+setup_init() {
+    if [ -n "$IS_RHEL" ]; then
+        chkconfig --add nmsa_handler.sh
+    elif [ -n "$IS_DEB" ] || [ -n "$IS_ANGSTROM" ]; then
+        update-rc.d nmsa_handler.sh defaults
+    elif [ -n "$IS_SLES" ]; then
+        /usr/lib/lsb/install_initd /etc/init.d/nmsa_handler.sh
+    elif [ -n "$IS_ESX" ]; then
+        # Yay! Manual labor!
+        mkdir -p /etc/rc.local.d/
+        ln -s /etc/init.d/nmsa_handler.sh /etc/rc.local.d/nmsa_handler.sh
+    else
+        # Undefined thing! This is very very bad!
+        echo "ERROR SETTING UP RC SCRIPTS! COULD NOT IDENTIFY DISTRIBUTION!" 2>&1 | tee $ERROR_LOG_FILE
+        echo "Check your installation logs and your distribution and try again!" 2>&1 | tee $ERROR_LOG_FILE
+        echo "NMSA Handler is NOT functioning properly!" 2>&1 | tee $ERROR_LOG_FILE
+    fi
+}
+
 # Clean up any previous installation, setup for new installation and install
 clean_up
 create_dir
 
 install_files
+
+setup_init
 
 # vim:set ai et sts=4 sw=4 tw=80:
