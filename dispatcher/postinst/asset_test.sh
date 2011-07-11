651496585a28b0805223c97f485d5f0fe0f627d2
Sam Hart <hartsn@gmail.com>
2011-07-11 12:26:02 -0400

    assetinfo test script
    --HG--
    branch : relayint

---
+++ dispatcher/postinst/asset_test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# asset_test.sh
+# -------------
+# Test script to determine if the system is capable of supporting the assetinfo
+# script.
+
+# THIS MUST ONLY BE RUN BY THE DISPATCHER INSTALL SCRIPT
+
+# Load our libraries
+. /opt/intel/eil/clientagent/lib/helper.sh
+. /opt/intel/eil/clientagent/lib/globals.sh
+
+# vim:set ai et sts=4 sw=4 tw=80:
