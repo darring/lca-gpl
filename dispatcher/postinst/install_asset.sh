5a7eb0a8a0373c8da35bd33f778408b78dd1c09c
Sam Hart <hartsn@gmail.com>
2011-07-11 13:25:14 -0400

    asset install script
    --HG--
    branch : relayint

---
+++ dispatcher/postinst/install_asset.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# install_asset.sh
+# --------------
+# Install and set up the assetinfo as a postinst process
+
+# Load our libraries
+. /opt/intel/eil/clientagent/lib/helper.sh
+. /opt/intel/eil/clientagent/lib/globals.sh
+
+# THIS MUST ONLY BE RUN BY THE DISPATCHER INSTALL SCRIPT
+
+# vim:set ai et sts=4 sw=4 tw=80:
