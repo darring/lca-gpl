af2a685317d698bbeff0bc92d1d5883c77512db5
Sam Hart <hartsn@gmail.com>
2011-07-11 12:54:09 -0400

    work on the test
    --HG--
    branch : relayint

--- dispatcher/postinst/asset_test.sh
+++ dispatcher/postinst/asset_test.sh
@@ -11,4 +11,48 @@
 . /opt/intel/eil/clientagent/lib/helper.sh
 . /opt/intel/eil/clientagent/lib/globals.sh
 
+INSTALL_DEPS=$(cat <<EOF
+bash
+ipmitool
+dmidecode
+dcmitool
+EOF
+)
+
+# FIXME - This is pretty ugly, any paths with ':" or "," in them will wreck it
+# however, it should be safe for now
+CLEAN_PATH=$(echo $PATH | sed "s/:/,/g" | sed -f ${LIB_DIR}/comma_split.sed)
+
+_check_file_in_path() {
+    RET_VAL=1
+    for P in $CLEAN_PATH
+    do
+        if [ -e "${P}/${1}" ]; then
+            RET_VAL=0
+            trace "asset_test: Found '${P}/${1}'...."
+            break
+        fi
+    done
+    return $RET_VAL
+}
+
+check_req_progs() {
+    RET_VAL=1
+    for APP_TO_CHECK in $INSTALL_DEPS
+    do
+        _check_file_in_path $APP_TO_CHECK
+        _STATUS=$?
+        if [ "${_STATUS}" -eq "0" ]; then
+            RET_VAL=0
+            break
+        fi
+    done
+    return $RET_VAL
+}
+
+trace "asset_test: Checking program requirements..."
+check_req_progs
+_STATUS=$?
+if [
+
 # vim:set ai et sts=4 sw=4 tw=80:
