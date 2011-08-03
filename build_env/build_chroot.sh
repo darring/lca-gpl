e2f52fb5defe06771fd1a74fd895ce84d4344b42
Sam Hart <hartsn@gmail.com>
2011-08-03 17:35:06 -0400

    further reworking of the build script
    --HG--
    branch : susebuild

--- build_env/build_chroot.sh
+++ build_env/build_chroot.sh
@@ -17,20 +17,10 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-# Check we're running debian/ubuntu
-if [ ! -f "/etc/debian_version" ]; then
-#     cat <<EOF
-
- This script must be run in a Debian-derived distribution (such as Ubuntu)!
-
- If you wish to build the EIL Linux client agent in another distribution, please
- see the BUILD_ENV text file and the source for this script to find the proper
- dependencies necessary to set up your build environment!
-
-EOF
-
-exit 1
-fi
+trace() {
+    DATESTAMP=$(date +'%Y-%m-%d %H:%M:%S %Z')
+    echo "${DATESTAMP} : ${*}"
+}
 
 deb_build() {
     CHROOT_PATH=$1
@@ -63,33 +53,24 @@ deb_build() {
 
 # Get our chroot path
 if [ -n "$1" ]; then
-    CHROOT_PATH=$1
-    debootstrap lucid ${CHROOT_PATH}
+    # determine which distro we're running
+    if [ -f "/etc/debian_version" ]; then
+        deb_build "${1}"
+    else if [ ! -f "/etc/SuSE-release" ];
+    else
+        cat <<EOF
 
-    # copy our items over into the chroot
-    cp -frv gsoap-2.8 ${CHROOT_PATH}/root/.
-    cp -fv setup_env.sh ${CHROOT_PATH}/root/.
-    chmod a+x ${CHROOT_PATH}/root/setup_env.sh
+ This script must be run in a Debian-derived distribution (such as Ubuntu) or
+ in an openSUSE-derived distribution!
 
-    # Mount our dev and proc
-    mount --bind /proc ${CHROOT_PATH}/proc
-    mount --bind /dev ${CHROOT_PATH}/dev
+ If you wish to build the EIL Linux client agent in another distribution, please
+ see the BUILD_ENV text file and the source for this script to find the proper
+ dependencies necessary to set up your build environment!
 
-    # Make sure we have universe and multiverse in our sources list
-    echo "deb http://archive.ubuntu.com/ubuntu lucid universe" \
-        >> ${CHROOT_PATH}/etc/apt/sources.list
-    echo "deb http://archive.ubuntu.com/ubuntu lucid multiverse" \
-        >> ${CHROOT_PATH}/etc/apt/sources.list
+EOF
 
-    # run the setup_env from the chroot
-    echo "#!/bin/sh" >> ${CHROOT_PATH}/root/run_once.sh
-    echo "cd /root" >> ${CHROOT_PATH}/root/run_once.sh
-    echo "./setup_env.sh" >> ${CHROOT_PATH}/root/run_once.sh
-
-    chmod a+x ${CHROOT_PATH}/root/run_once.sh
-
-    chroot ${CHROOT_PATH} /root/run_once.sh
-
+        exit 1
+    fi
     cat <<EOF
 
  Your chroot environment at "${CHROOT_PATH}" is now set up and ready for active
