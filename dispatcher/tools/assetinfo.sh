dbd864ea1edca9a8dd1318e5e45cfcf1e1bb148f
Stewart Dale ext:(%20stdalex%40UbuntuDev) <stewartx.t.dale@intel.com>
2011-11-15 02:28:33 -0800

    updates to asset collection script including the following:
    *) Check for a BMC KCS support (aka non supermicro systems) before
       starting tests.  If no KCS don't do tests that require ipmitool
       and auto mark as disabled (hopefully should cause this to start
       working on client systems).
    *) Updated XML format to move away from the <NMVersion>1.5</NMVersion>
       format to one using <Key>NMVersion</Key><Value>1.5</Value> so that
       CCMS doesn't choke and die when sending it in
    *) Inclusion of a few other minor changes to make asset collection
       on the slax image work better.  Any slax specific changes have
       if [ $slax ]; type checks around them so shouldn't cause issues on
       other systems.

--- dispatcher/tools/assetinfo.sh
+++ dispatcher/tools/assetinfo.sh
@@ -14,14 +14,29 @@
 # chkconfig: 2345 80 20
 # description: asset collection script
 
-IPMITOOL=ipmitool
-DCMITOOL=ipmitool
+# this check is slaxPxe specific, should
+# fail on any other time
+if [ -e /etc/rc.d/rc.myipmisetup ]; then 
+  /etc/rc.d/rc.myipmisetup &> /dev/null
+fi
+
+IPMITOOL=/usr/local/bin/ipmitool
+DCMITOOL=/usr/local/bin/ipmitool
 DMIDECODE=/usr/sbin/dmidecode
+
+DOIPMI=0
 
 FUNCT_ARRAY=("turbo" "hyperthreading" "vt" "vtd" "Xd" "UUID" "c6state" "c2c4state"
              "nmver" "dcmiver" "eist" "dimmslots" "dimmspop" "ramtotal" "dimm"
              "sriov" "niccount" "nicspeed" "fibercount" "fiberspeed" "sol"
              "remote" "hdcount" "raid" "oem")
+
+checkIpmi() { 
+ ipmitool mc info > /tmp/ipmitest;
+ if grep "Device ID" /tmp/ipmitest &> /dev/null; then
+  DOIPMI=1;
+ fi
+}
 
 printstart() {
  echo "<Attributes>"
@@ -31,200 +46,225 @@ printend() {
 }
 
 turbo() { #<Turbo>Enabled/Disabled</Turbo>
- echo "<Turbo>N/A</Turbo>" 
+ echo "<Key>Turbo</Key><Value>N/A</Value>" 
 }
 
 hyperthreading() { #<HyperThreading>Enabled/Disabled</HyperThreading>
- echo -n "<HyperThreading>"
+ echo -n "<Key>HyperThreading</Key><Value>"
  if grep ht /proc/cpuinfo &> /dev/null; then 
   echo -n "Enabled"
  else
   echo -n "Disabled"
  fi
- echo "</HyperThreading>"
+ echo "</Value>"
 }
 
 vt() { #<Vt>Enabled/Disabled</Vt>
- echo "<Vt>N/A</Vt>"
+ echo "<Key>Vt</Key><Value>N/A</Value>"
 }
 
 vtd() { #<Vt-d>Enabled/Disabled</Vt-d>
- echo "<Vt-d>N/A</Vt-d>"
+ echo "<Key>Vt-d</Key><Value>N/A</Value>"
 }
 
 Xd() { #<Xd>Enabled/Disabled</Xd>
- echo "<Xd>N/A</Xd>"
+ echo "<Key>Xd</Key><Value>N/A</Value>"
 }
 
 UUID() { #<UUID>Urbanna</UUID>                
- echo -n "<UUID>"
+ echo -n "<Key>UUID</Key><Value>"
  uuid=`$DMIDECODE| grep UUID | awk '{print $2}'`
  echo -n "$uuid"
- echo "</UUID>"
+ echo "</Value>"
 }
 
 c6state() { #<C6State>Enabled/Disabled</C6State>
- echo "<C6State>N/A</C6State>"
+ echo "<Key>C6State</Key><Value>N/A</Value>"
 }
 
 c2c4state() { #<C2C3>Enabled/Disabled</C2C3>
- echo "<C2C3>N/A</C2C3>"
+ echo "<Key>C2C3</Key><Value>N/A</Value>"
 }
 
 nmver() { #<NMVersion>1.5</NMVersion>
- echo -n "<NMVersion>"
- bmc_v=`$IPMITOOL mc info | grep IPMI | awk '{print $4}' 2>/dev/null`
- BMC_VER="Disabled"
- if [ "$bmc_v" == "2.0" ]; then
-  BMC_VER="1.5"
+ if [ $DOIPMI -eq 0 ]; then
+  echo "<Key>NMVersion</Key><Value>Disabled</Value>"
+ else
+  echo -n "<Key>NMVersion</Key><Value>"
+  bmc_v=`$IPMITOOL mc info | grep IPMI | awk '{print $4}' 2>/dev/null`
+  BMC_VER="Disabled"
+  if [ "$bmc_v" == "2.0" ]; then
+   BMC_VER="1.5"
+  fi
+  if [ "$bmc_v" == "2" ]; then
+   BMC_VER="1.5"
+  fi
+  if [ "$bmc_v" == "3.0" ]; then
+   BMC_VER="2.0"
+  fi
+  echo  -n "$BMC_VER"
+  echo "</Value>"
  fi
- if [ "$bmc_v" == "2" ]; then
-  BMC_VER="1.5"
- fi
- if [ "$bmc_v" == "3.0" ]; then
-  BMC_VER="2.0"
- fi
- echo  -n "$BMC_VER"
- echo "</NMVersion>"
 }
 
 dcmiver() { #<DCMIVersion>1.5</DCMIVersion>
- echo -n "<DCMIVersion>"
- v=`$DCMITOOL  raw 0x2c 0x01 0xdc 0x01 | awk '{print $2"."$3}'`
- echo -n "$v"
- echo "</DCMIVersion>"
+ if [ $DOIPMI -eq 0 ];then 
+  echo "<Key>DCMIVersion</Key><Value>Disabled</Value>"
+ else
+  echo -n "<Key>DCMIVersion</Key><Value>"
+  v=`$DCMITOOL  raw 0x2c 0x01 0xdc 0x01 | awk '{print $2"."$3}'`
+  echo -n "$v"
+  echo "</Value>"
+ fi
 }
 
 eist() { #<EIST>Enabled/Disabled</EIST>
- echo "<EIST>N/A</EIST>"
+ echo "<Key>EIST</Key><Value>N/A</Value>"
 }
 
 dimmslots() { #<DimmSlots>4</DimmSlots>
- echo -n "<DimmSlots>"
+ echo -n "<Key>DimmSlots</Key><Value>"
  cnt=`$DMIDECODE | grep "Memory Device" | wc -l`
  echo -n "$cnt"
- echo "</DimmSlots>"
+ echo "</Value>"
 }
 
 dimmspop() { #<DimmPopulated>4</DimmPopulated>
- echo -n "<DimmPopulated>"
+ echo -n "<Key>DimmPopulated</Key><Value>"
  cnt=`$DMIDECODE | grep "Memory Device" -A 17 | grep "Size" | grep -v "No Module Installed" | wc -l`
  echo -n "$cnt"
- echo "</DimmPopulated>"
+ echo "</Value>"
 }
 
 ramtotal() { #<RamTotal>12</RamTotal>
- echo -n "<RamTotal>"
+ echo -n "<Key>RamTotal</Key><Value>"
  result=`$DMIDECODE | grep "Memory Array" -A 6 | grep Range | awk '{print $3$4}'`
  echo -n "$result"
- echo "</RamTotal>"
+ echo "</Value>"
 }
 
 dimm() { #<Dimm><DimmSpeed>1066</DimmSpeed><DimmSize>2</DimmSize></Dimm>
- echo "<Dimm>"
- for dimm in `$DMIDECODE | grep "Memory Device" -A 17 | grep "Size" | grep -v "No Module" | awk '{print $2$3}'`; do
-  echo -n "<DimmSize>"
-  echo -n "$dimm"
-  echo    "</DimmSize>"  
+ echo -en "<Key>Dimm</Key>\n<Value>\n"
+ for dimm in `$DMIDECODE | grep "Memory Device" -A 17 | grep "Size" | grep -v "No Module" | grep -v "Range" | awk '{print $2$3}'`; do
+  echo -en "\t<Key>DimmSize</Key><Value>"
+  echo -en "$dimm"
+  echo    "</Value>"  
  done
- echo "</Dimm>"
+ echo "</Value>"
 }
 
 sriov() { #<SR-IOV>Enabled/Disabled</SR-IOV>
- echo "<SR-IOV>N/A</SR-IOV>"
+ echo "<Key>SR-IOV</Key><Value>N/A</Value>"
 }
 
 niccount() { #<NicCount>2</NicCount>
- echo -n "<NicCount>"
+ echo -n "<Key>NicCount</Key><Value>"
  cnt=`ifconfig -a| grep eth | wc -l`
  echo -n "$cnt"
- echo "</NicCount>"
+ echo "</Value>"
 }
 
 nicspeed() { #<NicSpeed>1Gb</NicSpeed>
  for i in "`dmesg | grep eth | grep Mbps`"; do
   speed=`echo $i | awk '{print $7 " " $8}'` 
-  echo "<NicSpeed>$speed</NicSpeed>"
+  echo "<Key>NicSpeed</Key><Value>$speed</Value>"
  done
 }
 
 FC_DIR=/sys/class/fc_host
 
 fibercount() { #<FiberCount>2</FiberCount>
- echo -n "<FiberCount>"
+ echo -n "<Key>FiberCount</Key><Value>"
  if [ ! -d $FC_DIR ]; then
   echo -n "0"
  else
   PORT_CNT=`ls -1 $FC_DIR| wc -l`
   echo -n "$PORT_CNT"
  fi
- echo "</FiberCount>"
+ echo "</Value>"
 }
 
 fiberspeed() { #<FiberSpeed>1Gb</FiberSpeed>
  if [ -d $FC_DIR ]; then 
   for i in `ls -1 $FC_DIR`; do
-   echo -n "<FiberSpeed>"
+   echo -n "<Key>FiberSpeed</Key><Value>"
    SPEEDS=`cat $FC_DIR/$i/supported_speeds`
    echo -n "$SPEEDS"
-   echo "</FiberSpeed>"
+   echo "</Value>"
   done
  else
-  echo "<FiberSpeed>Disabled</FiberSpeed>"
+  echo "<Key>FiberSpeed</Key><Value>Disabled</Value>"
  fi
 }
 
 sol() { #<SerialOverLan>Enabled/Disabled</SerialOverLan>
- echo -n "<SerialOverLan>"
- test=` $IPMITOOL sol info 1 | grep Enabled | awk '{print $3}'`
- if [ "$test" == "true" ]; then
-  echo -n "Enabled"
+ if [ $DOIPMI -eq 0 ]; then 
+  echo "<Key>SerialOverLan</Key><Value>Disabled</Value>"
  else
-  echo -n "Disabled"
+  echo -n "<Key>SerialOverLan</Key><Value>"
+  test=` $IPMITOOL sol info 1 | grep Enabled | awk '{print $3}'`
+  if [ "$test" == "true" ]; then
+   echo -n "Enabled"
+  else
+   echo -n "Disabled"
+  fi
+  echo "</Value>"
  fi
- echo "</SerialOverLan>"
 }
 
 remote() { #<RemoteCapability><IPaddress>10.19.253.1</IPaddress><Weblink>http://10.19.253.1</Weblink><iLO></iLO></RemoteCapability> 
- echo "<RemoteCapability>"
-  echo -n "<IPaddress>"
-  ip=`$IPMITOOL lan print | grep "IP Addr" | grep -v Source | awk '{print $4}'`
-  echo -n "$ip"
-  echo "</IPaddress>"
-  echo "<Weblink>http://$ip</Weblink>"
-  echo "<iLO></iLO>"
- echo "</RemoteCapability>"
+ echo -en "<Key>RemoteCapability</Key>\n<Value>\n"
+  echo -en "\t<Key>IPaddress</Key><Value>"
+  ip=""
+  if [ $DOIPMI -eq 0 ]; then
+   ip="N/A"
+  else
+   ip=`$IPMITOOL lan print | grep "IP Addr" | grep -v Source | awk '{print $4}'`
+  fi
+  echo -en "$ip"
+  echo "</Value>"
+  echo -e "\t<Key>Weblink</Key><Value>http://$ip</Value>"
+  echo -e "\t<Key>iLO</Key><Value>N/A</Value>"
+ echo "</Value>"
 }
 
 hdcount() { #<HardDriveCount>3</HardDriveCount>
- echo -n "<HardDriveCount>"
+ echo -n "<Key>HardDriveCount</Key><Value>"
  cnt=` fdisk -l | grep "^Disk" | cut -f 1 -d ',' | cut -f 3- -d '/' | wc -l`
  echo -n "$cnt"
- echo "</HardDriveCount>"
+ echo "</Value>"
 }
 
 raid() { #<Raid>Enabled/Disabled</Raid>
- echo -n "<Raid>"
- if dmesg | grep raid| grep PCI &> /dev/null; then 
-  echo -n "Enabled"
- else
-  echo -n "Disabled"
- fi
- echo "</Raid>"
+ echo  "<Key>Raid</Key><Value>Disabled</Value>"
+# if dmesg | grep raid| grep PCI &> /dev/null; then 
+#33333  echo -n "Enabled"
+# else
+#  echo -n "Disabled"
+#33 fi
+# echo "</Value>"
 }
 
 oem() { #<OEMInventory><Bios>1.5</Bios><BMC>2.11</BMC></OEMInventory>
- echo "<OEMInventory>"
- echo "<Bios>N/A</Bios>"
- echo -n "<BMC>"
- ver=` $IPMITOOL mc info | grep Firmware | grep -v Aux | awk '{print $4}'`
+ echo -en "<Key>OEMInventory</Key>\n<Value>\n\t"
+ echo -n "<Key>Bios</Key><Value>"
+ bv=`$DMIDECODE -s bios-version`
+ echo  "$bv</Value>"
+ echo -en "\t<Key>BMC</Key><Value>"
+ ver=""
+ if [ $DOIPMI -eq 0 ]; then
+  ver="N/A"
+ else
+  ver=` $IPMITOOL mc info | grep Firmware | grep -v Aux | awk '{print $4}'`
+ fi
  echo -n "$ver"
- echo "</BMC>"
+ echo "</Value>"
  
- echo "</OEMInventory>"
+ echo "</Value>"
 }
 
 collectInfo() {
+ checkIpmi
  if [ ! -d /opt/intel ]; then
   mkdir -p /opt/intel
  fi
