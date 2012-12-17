97cfc22bced2282ab3be1ce5068b0817c869a771
darrin (root@LENMP-58P) <none@none>
2012-12-17 10:11:37 -0500

    license clarification

--- steward/eil_steward.cpp
+++ steward/eil_steward.cpp
@@ -8,12 +8,6 @@
  * License Note:
  *  Please see the associated LICENSE file included with the source
  *  repository of this application for special license considerations.
- *  In a nutshell, parts of this application are based upon gSOAP, and
- *  depending upon how it is built (dynamic vs. static linking) it may
- *  also include GNU libc and other LGPL libraries. Care must be given
- *  in redistributing this application, its binary and its source, as
- *  redistribution outside of Intel will result in placing this software
- *  under the GPL for compliance.
  *
  */
 
